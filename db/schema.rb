cba727ecf6a03fba3303f54ae2564675a9e74c7b
Giovanni Cappellotto <potomak84@gmail.com>
2014-06-14 00:26:52 +0200

    Add interval and currency attributes to plans

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121207103300) do
+ActiveRecord::Schema.define(:version => 20140613220417) do
 
   create_table "plans", :force => true do |t|
     t.string   "paymill_id"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(:version => 20121207103300) do
     t.float    "price"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "interval"
+    t.string   "currency"
   end
 
   create_table "subscriptions", :force => true do |t|
